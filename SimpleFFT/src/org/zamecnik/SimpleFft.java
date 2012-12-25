fd1bef65a0c94fc27e91d2d730d59d75490b27f7
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2012-12-25 13:26:15 +0100

    Normalize and chop the results of an FFT.

--- SimpleFFT/src/org/zamecnik/SimpleFft.java
+++ SimpleFFT/src/org/zamecnik/SimpleFft.java
@@ -1,6 +1,6 @@
 package org.zamecnik;
 
-import edu.emory.mathcs.jtransforms.fft.*;
+import edu.emory.mathcs.jtransforms.fft.FloatFFT_1D;
 
 public class SimpleFft {
 	private static final int DATA_SIZE = 128;
@@ -12,8 +12,18 @@ public class SimpleFft {
 
 		FloatFFT_1D fft = new FloatFFT_1D(DATA_SIZE);
 		fft.realForward(data);
+		normalize(data);
+		chop(data);
 
 		System.out.println("FFT-transformed values: " + print(data));
+	}
+
+	private static void normalize(float[] spectrum) {
+		float sizeInverse = 1.0f / DATA_SIZE;
+		for (int i = 0; i < spectrum.length; i++) {
+			spectrum[i] = 2 * spectrum[i] * sizeInverse;
+		}
+		spectrum[0] *= 0.5; // DC component (zero frequency)
 	}
 
 	private static float[] generateCosineWave() {
@@ -24,6 +34,18 @@ public class SimpleFft {
 		return data;
 	}
 
+	private static void chop(float[] data) {
+		chop(data, 10e-8f);
+	}
+	
+	private static void chop(float[] data, float threshold) {
+		for (int i = 0; i < data.length; i++) {
+			if (Math.abs(data[i]) < threshold) {
+				data[i] = 0;
+			}
+		}
+	}
+	
 	private static String print(float[] array) {
 		StringBuilder sb = new StringBuilder("[");
 		for (int i = 0; i < array.length; i++) {
