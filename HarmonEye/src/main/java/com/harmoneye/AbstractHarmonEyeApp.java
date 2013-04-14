8517feb4152006449b43f20f439c9d35e2c5731a
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2013-04-14 19:40:56 +0200

    Switch the circle-of-fifths mode via an action on a menu item.

--- HarmonEye/src/main/java/com/harmoneye/AbstractHarmonEyeApp.java
+++ HarmonEye/src/main/java/com/harmoneye/AbstractHarmonEyeApp.java
@@ -4,10 +4,16 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+import javax.swing.AbstractAction;
+import javax.swing.ImageIcon;
+import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
@@ -19,6 +25,9 @@ public class AbstractHarmonEyeApp extends JPanel implements ActionListener {
 	private Timer timer;
 	protected MusicAnalyzer soundAnalyzer;
 
+	private CircleOfFifthsEnabledAction circleOfFifthsEnabledAction;
+	private JCheckBoxMenuItem circleOfFifthsEnabledMenuItem;
+
 	public AbstractHarmonEyeApp() {
 		timer = new Timer(TIME_PERIOD_MILLIS, this);
 		timer.setInitialDelay(190);
@@ -29,10 +38,30 @@ public class AbstractHarmonEyeApp extends JPanel implements ActionListener {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(512, 512);
 		frame.setLocationRelativeTo(null);
+
+		circleOfFifthsEnabledAction = new CircleOfFifthsEnabledAction(
+				"Circle of fifths", null, "", new Integer(KeyEvent.VK_F));
+
+		frame.setJMenuBar(createMenuBar());
+
 		frame.setVisible(true);
 
 		// enable pausing
 		this.addMouseListener(new MouseClickListener());
+	}
+
+	private JMenuBar createMenuBar() {
+		JMenu mainMenu = new JMenu("Settings");
+
+		circleOfFifthsEnabledMenuItem = new JCheckBoxMenuItem(
+				circleOfFifthsEnabledAction);
+		circleOfFifthsEnabledMenuItem.setSelected(false);
+		circleOfFifthsEnabledMenuItem.setIcon(null);
+		mainMenu.add(circleOfFifthsEnabledMenuItem);
+
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.add(mainMenu);
+		return menuBar;
 	}
 
 	public void start() {
@@ -78,4 +107,20 @@ public class AbstractHarmonEyeApp extends JPanel implements ActionListener {
 		}
 	}
 
+	private class CircleOfFifthsEnabledAction extends AbstractAction {
+		private static final long serialVersionUID = 1L;
+
+		public CircleOfFifthsEnabledAction(String text, ImageIcon icon,
+				String desc, Integer mnemonic) {
+			super(text, icon);
+			putValue(SHORT_DESCRIPTION, desc);
+			putValue(MNEMONIC_KEY, mnemonic);
+		}
+
+		public void actionPerformed(ActionEvent e) {
+			boolean fifthsEnabled = circleOfFifthsEnabledMenuItem.getState();
+			soundAnalyzer.getVisualizer().setPitchStep(fifthsEnabled ? 7 : 1);
+			repaint();
+		}
+	}
 }
