2f651c04a088904f1068e678c5cafd7c6c63e9e0
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2013-04-14 19:41:38 +0200

    Use the native Mac look-and-feel for the main menu.
    Use the system menu bar instead of the one inside the window.

--- HarmonEye/src/main/java/com/harmoneye/HarmonEye.java
+++ HarmonEye/src/main/java/com/harmoneye/HarmonEye.java
@@ -1,7 +1,30 @@
 package com.harmoneye;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 public class HarmonEye {
 	public static void main(String[] args) {
-		new CaptureHarmonEyeApp().start();
+		try {
+			System.setProperty("apple.laf.useScreenMenuBar", "true");
+			System.setProperty(
+					"com.apple.mrj.application.apple.menu.about.name",
+					"HarmonEye");
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException e) {
+			System.out.println("ClassNotFoundException: " + e.getMessage());
+		} catch (InstantiationException e) {
+			System.out.println("InstantiationException: " + e.getMessage());
+		} catch (IllegalAccessException e) {
+			System.out.println("IllegalAccessException: " + e.getMessage());
+		} catch (UnsupportedLookAndFeelException e) {
+			System.out.println("UnsupportedLookAndFeelException: "
+					+ e.getMessage());
+		}
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new CaptureHarmonEyeApp().start();
+			}
+		});
 	}
 }
