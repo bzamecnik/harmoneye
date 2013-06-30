179515d32dbdb4067f1c99c9ab9fabaeff2b00f4
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2013-06-30 17:52:50 +0200

    Refactor a variable to a constant.

--- HarmonEye/src/main/java/com/harmoneye/util/ByteConverter.java
+++ HarmonEye/src/main/java/com/harmoneye/util/ByteConverter.java
@@ -5,10 +5,11 @@ import java.nio.ByteOrder;
 
 public class ByteConverter {
 
+	// signed short to [-1; 1]
+	private static final double normalizationFactor = 2 / (double) 0xffff;
+
 	public static void littleEndianBytesToDoubles(byte[] bytes, double[] doubles) {
 		assert bytes.length == 2 * doubles.length;
-		// signed short to [-1; 1]
-		double normalizationFactor = 2 / (double) 0xffff;
 		ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
 		byteBuffer.order(ByteOrder.LITTLE_ENDIAN);
 		for (int i = 0; i < doubles.length && byteBuffer.hasRemaining(); i++) {
