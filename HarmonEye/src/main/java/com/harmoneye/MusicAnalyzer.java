d60c7c39947d1082b0548c738c4cee514cc3ff48
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2013-12-03 18:43:54 +0100

    Extract some components into separate files.

--- HarmonEye/src/main/java/com/harmoneye/MusicAnalyzer.java
+++ HarmonEye/src/main/java/com/harmoneye/MusicAnalyzer.java
@@ -130,58 +130,6 @@ public class MusicAnalyzer implements SoundConsumer {
 		return signalBlockSize;
 	}
 
-	private static class ExpSmoother {
-		double[] data;
-		double currentWeight;
-		double previousWeight;
-
-		public ExpSmoother(int size, double currentWeight) {
-			data = new double[size];
-			this.currentWeight = currentWeight;
-			previousWeight = 1 - currentWeight;
-		}
-
-		public double[] smooth(double[] currentFrame) {
-			assert data.length == currentFrame.length;
-
-			for (int i = 0; i < data.length; i++) {
-				data[i] = previousWeight * data[i] + currentWeight * currentFrame[i];
-			}
-			return data;
-		}
-	}
-
-	private static class MovingAverageAccumulator {
-		double[] data;
-		int frameCount;
-
-		public MovingAverageAccumulator(int size) {
-			data = new double[size];
-		}
-
-		public double[] getAverage() {
-			return data;
-		}
-
-		public double[] add(double[] currentFrame) {
-			assert data.length == currentFrame.length;
-
-			frameCount++;
-
-			double weight = 1.0 / frameCount;
-
-			for (int i = 0; i < data.length; i++) {
-				data[i] = (1 - weight) * data[i] + weight * currentFrame[i];
-			}
-
-			return data;
-		}
-
-		public void reset() {
-			frameCount = 0;
-		}
-	}
-
 	public void toggleAccumulatorEnabled() {
 		accumulatorEnabled = !accumulatorEnabled;
 		if (accumulatorEnabled) {
