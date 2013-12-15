4d170c5b833ff448bf654cde56c7ea5ede1eec36
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2013-12-15 16:03:42 +0100

    Refactor ToneGenerator and add a test.

--- HarmonEye/src/test/java/com/harmoneye/audio/ToneGenerator.java
+++ HarmonEye/src/test/java/com/harmoneye/audio/ToneGenerator.java
@@ -15,14 +15,72 @@ public class ToneGenerator {
 	 * @return
 	 */
 	public double[] generateSinWave(double frequency, double length) {
+		return generateSignal(new Sine(frequency, 1.0), length);
+	}
+	
+	public double[] generateSignal(Function1D function, double length) {
 		int sampleCount = (int) Math.ceil(length * samplingFreq);
+		double timeStep = 1.0 / samplingFreq;
 		double[] signal = new double[sampleCount];
-		double xStep = 2 * Math.PI * frequency / samplingFreq;
-		double x = 0;
-		for (int i = 0; i < sampleCount; i++, x += xStep) {
-			signal[i] = Math.sin(x);
+		for (int i = 0; i < sampleCount; i++) {
+			signal[i] = function.value(i * timeStep);
 		}
 		return signal;
 	}
 
+	interface Function1D {
+		double value(double time);
+	}
+
+	static class Sine implements Function1D {
+		private double xStep;
+		private double amplitude;
+
+		public Sine(double frequency, double amplitude) {
+			this.xStep = 2 * Math.PI * frequency;
+			this.amplitude = amplitude;
+		}
+
+		@Override
+		public double value(double time) {
+			return amplitude * Math.sin(time * xStep);
+		}
+	}
+
+	static class Sum implements Function1D {
+		private Function1D[] functions;
+
+		public Sum(Function1D[] functions) {
+			super();
+			this.functions = functions;
+		}
+
+		@Override
+		public double value(double time) {
+			double sum = 0;
+			for (int i = 0; i < functions.length; i++) {
+				sum += functions[i].value(time);
+			}
+			return sum;
+		}
+	}
+	
+	static class HarmonicSines implements Function1D {
+		private Sine fundamental;
+		private int harmonicCount;
+
+		public HarmonicSines(Sine fundamental, int harmonicCount) {
+			this.fundamental = fundamental;
+			this.harmonicCount = harmonicCount;
+		}
+
+		@Override
+		public double value(double time) {
+			double sum = 0;
+			for (int i = 0; i < harmonicCount; i++) {
+				sum += fundamental.value(time / i);
+			}
+			return sum;
+		}
+	}
 }
---
+++ HarmonEye/src/test/java/com/harmoneye/audio/ToneGeneratorTest.java
@@ -0,0 +1,37 @@
+package com.harmoneye.audio;
+
+import static com.harmoneye.audio.TextSignalPrinter.printSignal;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ToneGeneratorTest {
+
+	@Test
+	public void generateUnitSignal() {
+		ToneGenerator toneGenerator = new ToneGenerator(20.0);
+		double[] signal = toneGenerator.generateSinWave(1.0, 1.0);
+
+		printSignal(signal);
+
+		double[] expectedSignal = new double[] { 0.0, 0.3090169943749474,
+			0.5877852522924731, 0.8090169943749475, 0.9510565162951535, 1.0,
+			0.9510565162951535, 0.8090169943749475, 0.5877852522924732,
+			0.3090169943749475, 1.2246467991473532E-16, -0.30901699437494773,
+			-0.5877852522924734, -0.8090169943749473, -0.9510565162951535,
+			-1.0, -0.9510565162951536, -0.809016994374947, -0.5877852522924734,
+			-0.3090169943749476 };
+		assertEquals(expectedSignal.length, signal.length);
+		for (int i = 0; i < expectedSignal.length; i++) {
+			assertEquals(expectedSignal[i], signal[i], 1e-6);
+		}
+	}
+	
+	@Test
+	public void generateSomeSignal() {
+		ToneGenerator toneGenerator = new ToneGenerator(20.0);
+		double[] signal = toneGenerator.generateSinWave(5.0, 2.0);
+
+		printSignal(signal);
+	}
+}
