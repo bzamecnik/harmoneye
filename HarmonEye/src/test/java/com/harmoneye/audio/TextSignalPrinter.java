76a124d67f895242e3b462cbe15db8a9de07894d
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2013-12-16 13:49:06 +0100

    Make some manual test of temporal and spectral kernels.

--- HarmonEye/src/test/java/com/harmoneye/audio/TextSignalPrinter.java
+++ HarmonEye/src/test/java/com/harmoneye/audio/TextSignalPrinter.java
@@ -1,14 +1,46 @@
 package com.harmoneye.audio;
 
+import org.apache.commons.math3.complex.Complex;
+
 
 public class TextSignalPrinter {
 	public static void printSignal(double[] signal) {
-		System.out.println("signal:");
-		System.out.println("length: " + signal.length);
+		System.out.println("signal of length: " + signal.length);
 		for (int i = 0; i < signal.length; i++) {
-			System.out.println(toStars(signal[i]) + "\t" + signal[i]);
+			printLine(signal[i]);
 		}
 		System.out.println();
+	}
+	
+	public static void printSignal(Complex[] signal) {
+		System.out.println("signal of length: " + signal.length);
+		
+		double max = Double.NEGATIVE_INFINITY;
+		for (int i = 0; i < signal.length; i++) {
+			double abs = signal[i].abs();
+			max = Math.max(max, abs);
+		};
+		System.out.println("max: " + max);
+		double scaleFactor = 1.0 / max;
+		
+		System.out.println("abs: ");
+		for (int i = 0; i < signal.length; i++) {
+			printLine(signal[i].abs() * scaleFactor);
+		}
+		System.out.println("real component: ");
+		for (int i = 0; i < signal.length; i++) {
+			printLine(signal[i].getReal() * scaleFactor);
+		}
+		System.out.println("imaginary component: ");
+		for (int i = 0; i < signal.length; i++) {
+			printLine(signal[i].getImaginary() * scaleFactor);
+		}
+		System.out.println();
+	}
+	
+	private static void printLine(double value) {
+		System.out.println(toStars(value) + "\t" + value);
+//		System.out.println(value);
 	}
 
 	private static String toStars(double amplitude) {
