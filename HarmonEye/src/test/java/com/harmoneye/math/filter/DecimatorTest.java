6717ee89c61442d45182c0dcfe9021a9653908e6
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2014-01-19 19:42:38 +0100

    Create a manual test for Decimator.

---
+++ HarmonEye/src/test/java/com/harmoneye/math/filter/DecimatorTest.java
@@ -0,0 +1,34 @@
+package com.harmoneye.math.filter;
+
+import static com.harmoneye.audio.TextSignalPrinter.printSignal;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DecimatorTest {
+
+	@Test
+	public void test() {
+		Filter filter = new ZeroPhaseFilter(new ButterworthFilter());
+		Decimator decimator = new Decimator(filter);
+		
+		double[] signal = new double[256];
+		for (int i = 0; i < signal.length; i++) {
+			signal[i] = 10 + 0.1 * Math.random();
+		}
+		double [] signal1 = new double[signal.length / 2];
+		System.arraycopy(signal, 0, signal1, 0, signal1.length);
+		double [] signal2 = new double[signal.length / 2];
+		System.arraycopy(signal, 0, signal2, 0, signal2.length);
+		
+		//printSignal(signal);
+		
+		printSignal(filter.filter(signal1));
+		printSignal(filter.filter(signal2));
+		
+		double[] decimated = decimator.decimate(signal);
+		
+		//printSignal(decimated);
+	}
+
+}
