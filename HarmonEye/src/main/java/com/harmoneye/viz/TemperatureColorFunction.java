3781ae796ca0f6f27e8741d713322acb5f2c6d91
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2014-01-22 14:42:33 +0100

    Colorize weak tones with less saturation.

--- HarmonEye/src/main/java/com/harmoneye/viz/TemperatureColorFunction.java
+++ HarmonEye/src/main/java/com/harmoneye/viz/TemperatureColorFunction.java
@@ -6,7 +6,10 @@ public class TemperatureColorFunction implements ColorFunction {
 
 	public Color toColor(float value) {
 		float hue = (1.8f - value) % 1.0f;
-		return Color.getHSBColor(hue, 0.25f + 0.75f * value, 0.25f + 0.75f * value);
+		//float saturation = 0.75f * value + 0.25f * value * value;
+		float saturation = value > 0.5f ? value : 0.05f + value * value;
+		float brightness = 0.25f + 0.75f * value;
+		return Color.getHSBColor(hue, saturation, brightness);
 	}
 
 }
