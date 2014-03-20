e2402e539a520c4ee9ca16ea627e4fb00d7129c9
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2014-03-21 00:38:24 +0100

    Change the ring buffer implementation.
    Add an read index - from where the next read() should begin. It can be modified independently from the read() operation.
    Use System.arraycopy() instead of per-element copying.

--- HarmonEye/src/main/java/com/harmoneye/audio/DoubleRingBuffer.java
+++ HarmonEye/src/main/java/com/harmoneye/audio/DoubleRingBuffer.java
@@ -1,5 +1,7 @@
 package com.harmoneye.audio;
 
+import java.util.Arrays;
+
 /**
  * Lock-free ring buffer.
  * 
@@ -9,7 +11,8 @@ public class DoubleRingBuffer {
 
 	private final int bufferSize;
 	private double[] buffer;
-	private volatile int endIndex = 0;
+	private volatile int writeIndex = 0;
+	private volatile int readIndex = 0;
 
 	public DoubleRingBuffer(int bufferSize) {
 		this.bufferSize = bufferSize;
@@ -23,13 +26,16 @@ public class DoubleRingBuffer {
 	 * 
 	 * @param samples
 	 */
-	public void write(double[] samples) {
-		int index = endIndex;
-		endIndex = incrementIndex(endIndex, samples.length);
-		for (int i = 0; i < samples.length; i++) {
-			buffer[index] = samples[i];
-			index = incrementIndex(index, 1);
-		}
+	public void write(double[] values) {
+		// println("wrappedDestCopy(values, 0, buffer, writeIndex="+writeIndex+", values.length="+values.length+")");
+		wrappedDestCopy(values, 0, buffer, writeIndex, values.length);
+		writeIndex = mod(writeIndex + values.length);
+	}
+
+	/** Reads data from the read index. The read index itself is not updated. */
+	public double[] read(int length, double[] result) {
+		read(length, readIndex, result);
+		return result;
 	}
 
 	/**
@@ -39,14 +45,65 @@ public class DoubleRingBuffer {
 	 * @param result
 	 */
 	public void readLast(int length, double[] result) {
-		int index = incrementIndex(endIndex, -length);
-		for (int i = 0; i < length; i++) {
-			result[i] = buffer[index];
-			index = incrementIndex(index, 1);
+		read(length, mod(writeIndex - length), result);
+	}
+
+	private double[] read(int length, int fromIndex, double[] result) {
+		// println("wrappedSrcCopy(buffer, readIndex="+readIndex+", result, 0, length="+length+");");
+		wrappedSrcCopy(buffer, readIndex, result, 0, length);
+		return result;
+	}
+
+	public void incrementReadIndex(int increment) {
+		readIndex = mod(readIndex + increment);
+	}
+
+	public int getCapacityForRead() {
+		return mod(writeIndex - readIndex);
+	}
+
+	private void wrappedSrcCopy(double[] src, int srcPos, double[] dest,
+		int destPos, int length) {
+		int firstBlockLength = Math.min(length, src.length - srcPos);
+		int secondBlockLength = length - firstBlockLength;
+		// println("first: srcPos=" + srcPos + ", destPos=" + destPos +
+		// " length=" + firstBlockLength);
+		System.arraycopy(src, srcPos, dest, destPos, firstBlockLength);
+		if (secondBlockLength > 0) {
+			// println("second: srcPos=" + 0 + ", destPos=" + firstBlockLength +
+			// ", length=" + secondBlockLength);
+			System.arraycopy(src, 0, dest, firstBlockLength, secondBlockLength);
 		}
 	}
 
-	private int incrementIndex(int value, int increment) {
-		return (((value + increment) % bufferSize) + bufferSize) % bufferSize;
+	private void wrappedDestCopy(double[] src, int srcPos, double[] dest,
+		int destPos, int length) {
+		int firstBlockLength = Math.min(length, dest.length - destPos);
+		int secondBlockLength = length - firstBlockLength;
+		// println("first: srcPos=" + srcPos + ", destPos=" + destPos +
+		// ", length=" + firstBlockLength);
+		System.arraycopy(src, srcPos, dest, destPos, firstBlockLength);
+		if (secondBlockLength > 0) {
+			// println("second: srcPos=" + firstBlockLength + ", destPos=" + 0 +
+			// ", length=" + secondBlockLength);
+			System.arraycopy(src, firstBlockLength, dest, 0, secondBlockLength);
+		}
 	}
+
+	private int mod(int value) {
+		return ((value % bufferSize) + bufferSize) % bufferSize;
+	}
+
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("[");
+		sb.append("size: ").append(bufferSize);
+		sb.append(", write: ").append(writeIndex);
+		sb.append(", read: ").append(readIndex);
+		sb.append(", read capacity: ").append(getCapacityForRead());
+		sb.append(", buffer: ").append(Arrays.toString(buffer));
+		sb.append("]");
+		return sb.toString();
+	}
+
 }
