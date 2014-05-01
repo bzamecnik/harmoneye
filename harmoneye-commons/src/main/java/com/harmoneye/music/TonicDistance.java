4f4b5b73b9d092b0ff8839af8dd1956c3b9d93f0
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2014-05-01 23:26:39 +0200

    Implement a chord timeline visualizer.

--- harmoneye-commons/src/main/java/com/harmoneye/music/TonicDistance.java
+++ harmoneye-commons/src/main/java/com/harmoneye/music/TonicDistance.java
@@ -1,12 +1,14 @@
 package com.harmoneye.music;
 
 /**
- * Calculates distance of a tone to given tonic (both represented as continuous
+ * Calculates "distance" of a tone to given tonic (both represented as continuous
  * pitch classes. The values rises from the tonic via circle of fifths and in
  * the middle there is a skip and it continoues down the rest of the circle of
  * fifths.
  * 
  * Eg. for the tonic of C the distances rises as follows from 0.0 to 1.0:
+ * 
+ * In fact it is not true distance, since it is not symmetric.
  * 
  * C, G, D, A, E, B, F, Bb, Eb, Ab, Db, Gb
  */
@@ -18,9 +20,16 @@ public class TonicDistance {
 		this.octaveSize = octaveSize;
 	}
 
+	// normalized float distance [0.0; 1.0]
 	public float distance(float tone, int tonic) {
 		float i = ((tone - tonic + octaveSize) * 7) % octaveSize;
 		return (i < 6 ? i : octaveSize - 1 + 6 - i) / (octaveSize - 1.0f);
+	}
+	
+	// unnormalized integer distance [0; octaveSize - 1]
+	public int distanceInt(int tone, int tonic) {
+		int i = ((tone - tonic + octaveSize) * 7) % octaveSize;
+		return (i < 6 ? i : octaveSize - 1 + 6 - i);
 	}
 
 	/**
---
+++ harmoneye-experiments/src/main/java/com/harmoneye/app/chordtimeline/ChordTimeLineApp.java
@@ -0,0 +1,258 @@
+package com.harmoneye.app.chordtimeline;
+
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+import processing.core.PApplet;
+import sojamo.drop.DropEvent;
+import sojamo.drop.SDrop;
+
+import com.harmoneye.music.KeyAwarePitchClassSetIndexer;
+import com.harmoneye.music.PitchClassNamer;
+import com.harmoneye.music.PitchClassSet;
+import com.harmoneye.music.PitchClassSetNamer;
+import com.harmoneye.music.TonicDistance;
+import com.harmoneye.music.chord.ChordLabel;
+import com.harmoneye.music.chord.ChordLabels;
+import com.harmoneye.music.chord.TimedChordLabel;
+
+public class ChordTimeLineApp extends PApplet {
+
+	private static final long serialVersionUID = 6524424444348489408L;
+
+	String dir = "/Users/bzamecnik/Documents/harmoneye-labs/music-information-retrieval/columbia-music-signal-processing/data/beatles/chordlabs/";
+	String fileName = dir + "A_Hard_Day_s_Night/05-And_I_Love_Her.lab";
+
+	SDrop sdrop;
+
+	List<TimedChordLabel> chordLabels = new ArrayList<TimedChordLabel>();
+	double totalTime;
+	int tonic = 4;
+
+	TonicDistance tonicDist = new TonicDistance(12);
+	KeyAwarePitchClassSetIndexer chordIndexer = new KeyAwarePitchClassSetIndexer();
+
+	PitchClassNamer pitchClassNamer = PitchClassNamer.defaultInstance();
+
+	String MODE_LINEAR = "LINEAR";
+	String MODE_FIFTHS = "FIFTHS";
+	String MODE_TONIC_DIST = "TONIC_DIST";
+
+	String mode = MODE_TONIC_DIST;
+	int diatonicStep = 1;
+
+	public static void main(String[] args) {
+		ChordTimeLineApp applet = new ChordTimeLineApp();
+
+		PApplet.runSketch((String[]) PApplet
+			.concat(new String[] { ChordTimeLineApp.class.getName() }, args),
+			applet);
+	}
+
+	public void setup() {
+		size(1000, 200);
+
+		sdrop = new SDrop(this);
+		loadFile(fileName);
+
+		// println(chordLabels);
+		// println(totalTime);
+
+		colorMode(HSB, 1.0f);
+
+		noLoop();
+	}
+
+	public void draw() {
+		background(0.15f);
+
+		translate(0, height);
+		scale(1, -1);
+
+		float xScale = (float) (width / totalTime);
+		float yScale = height / 12.0f;
+
+		rectMode(CORNER);
+
+		fill(0.2f);
+		noStroke();
+		if (mode.equals(MODE_TONIC_DIST)) {
+			fill(0, 0.2f, 0.15f);
+			rect(0, 7 * yScale, width, 5 * yScale);
+			fill(0.125f, 0.2f, 0.2f);
+			rect(0, 5 * yScale, width, 2 * yScale);
+			fill(0.25f, 0.2f, 0.2f);
+			rect(0, 0, width, 5 * yScale);
+			fill(0.25f, 0.2f, 0.3f);
+			rect(0, 0, width, yScale);
+		} else if (mode.equals(MODE_LINEAR)) {
+			for (int i = 0; i < 12; i++) {
+				int t = ((i - tonic + 12)) % 12;
+				if (tonicDist.distanceInt(i, 0) < 7) {
+					fill(0.25f, 0.2f, i == 0 ? 0.3f : 0.2f);
+					rect(0, i * yScale, width, yScale);
+				}
+			}
+		}
+
+		stroke(0);
+
+		for (TimedChordLabel label : chordLabels) {
+			float xStart = (float) (label.getStartTime() * xScale);
+			float xEnd = (float) (label.getEndTime() * xScale);
+			ChordLabel chordLabel = label.getChordLabel();
+			List<Integer> tones = chordLabel.getTones();
+			if (!tones.isEmpty()) {
+				// float dist = tonicDist.distance(chordLabel.getRoot(), tonic);
+				// float dist = (float)chordIndexer.getLogIndex(tones);
+				// //println(dist);
+				// fill(tonicDist.distanceToHue(dist), 0.5f, 1);
+				// float xSize = xEnd - xStart;
+				// if (xSize <= 2.0f) {
+				// noStroke();
+				// } else {
+				// stroke(0);
+				// }
+				// rect(xStart, height*(1-dist), xSize, height);
+				int root = chordLabel.getRoot();
+				for (Integer tone : tones) {
+					fill(tonicDist.distanceToHue(tonicDist
+						.distance(tone, tonic)), 0.5f, tone == root ? 1 : 0.5f);
+					int t = tone;
+					if (mode.equals(MODE_LINEAR)) {
+						t = ((tone - tonic + 12)) % 12;
+					}
+					// int t = ((tone - tonic + 12) * 7 + 1) % 12;
+					else if (mode.equals(MODE_TONIC_DIST)) {
+						t = tonicDist.distanceInt(tone, tonic);
+						if (t < 7) {
+							t = (t * diatonicStep) % 7;
+						}// else {
+							// t = (((t - 7) * 2) % 7) + 7;
+							// }
+					}
+					float xSize = xEnd - xStart;
+					if (xSize <= 2.0f) {
+						noStroke();
+					} else {
+						stroke(0);
+					}
+					rect(xStart, t * yScale, xSize, yScale);
+				}
+			}
+		}
+	}
+
+	public void keyPressed() {
+		if (key == 'w') {
+			shiftTonic(7);
+		} else if (key == 's') {
+			shiftTonic(5);
+		} else if (key == 'q') {
+			shiftTonic(1);
+		} else if (key == 'a') {
+			shiftTonic(11);
+		} else if (key == 'm') {
+			if (mode.equals(MODE_LINEAR)) {
+				mode = MODE_TONIC_DIST;
+				redraw();
+			} else if (mode.equals(MODE_TONIC_DIST)) {
+				mode = MODE_LINEAR;
+				redraw();
+			}
+		} else if (key == 'd') {
+			diatonicStep = 1 + (diatonicStep % 2);
+			redraw();
+		}
+	}
+
+	public void mouseClicked() {
+		double time = totalTime * mouseX / width;
+		int labelCount = chordLabels.size();
+		for (int i = 0; i < labelCount; i++) {
+			TimedChordLabel label = chordLabels.get(i);
+			if (label.getEndTime() > time) {
+				ChordLabel chordLabel = label.getChordLabel();
+				List<Integer> tones = chordLabel.getTones();
+				String chordTitle = chordLabel.getTitle();
+				println(time + " " + chordTitle + " " + tones);
+				break;
+			}
+		}
+	}
+
+	void shiftTonic(int offset) {
+		setTonic((tonic + offset) % 12);
+		redraw();
+	}
+
+	void loadFile(String fileName) {
+		if (!fileName.endsWith(".lab")) {
+			println("Unsupported file format: " + fileName);
+			return;
+		}
+
+		this.fileName = fileName;
+		try {
+			Reader reader = createReader(fileName);
+			chordLabels = new ChordLabels().parseTimedChords(reader);
+			if (!chordLabels.isEmpty()) {
+				totalTime = chordLabels.get(chordLabels.size() - 1)
+					.getEndTime();
+				setTonic(findTonic(chordLabels));
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			// exit();
+		}
+
+		redraw();
+	}
+
+	void setTonic(int tonic) {
+		this.tonic = tonic;
+		tonicChanged();
+	}
+
+	void tonicChanged() {
+		frame.setTitle(fileName.replaceAll(".*/", "") + " (key: "
+			+ pitchClassNamer.getName(tonic) + ")");
+	}
+
+	public void dropEvent(DropEvent event) {
+		if (event.isFile()) {
+			loadFile(event.toString());
+		}
+	}
+
+	int findTonic(List<TimedChordLabel> labels) {
+		long[] keyIndexSums = new long[12];
+		int size = labels.size();
+		// try to find the first key in case of later modulations
+		int searchedSize = (int) Math.round(size * 0.25);
+		for (int i = 0; i < searchedSize; i++) {
+			TimedChordLabel label = labels.get(i);
+			List<Integer> tones = label.getChordLabel().getTones();
+			PitchClassSet pcs = PitchClassSet.fromSet(new HashSet<Integer>(
+				tones));
+			for (int tonic = 0; tonic < 12; tonic++) {
+				keyIndexSums[tonic] += chordIndexer.getIndex(pcs, tonic);
+			}
+		}
+		println("keyIndexSums: " + Arrays.toString(keyIndexSums));
+		int minKeyIndex = 0;
+		long keyIndexSum = Integer.MAX_VALUE;
+		for (int i = 0; i < 12; i++) {
+			if (keyIndexSums[i] < keyIndexSum) {
+				keyIndexSum = keyIndexSums[i];
+				minKeyIndex = i;
+			}
+		}
+		println("best key: " + minKeyIndex);
+		return minKeyIndex;
+	}
+
+}
