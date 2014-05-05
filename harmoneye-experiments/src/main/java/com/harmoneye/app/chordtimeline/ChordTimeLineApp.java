ee6d8c88e532f4cbbcf4fa4db21bd30fefa1dd71
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2014-05-05 19:02:45 +0200

    Add synchronized audio playback to the chord time line app.
    Allow to play, pause, stop, rewind to selected position.
    Highlight the current chord. Show its symbolic name.
    Allow an audio file to be dropped onto the window.

--- harmoneye-experiments/src/main/java/com/harmoneye/app/chordtimeline/ChordTimeLineApp.java
+++ harmoneye-experiments/src/main/java/com/harmoneye/app/chordtimeline/ChordTimeLineApp.java
@@ -18,26 +18,39 @@ import com.harmoneye.music.chord.ChordLabels;
 import com.harmoneye.music.chord.TimedChordLabel;
 import com.harmoneye.p5.PanZoomController;
 
+import ddf.minim.AudioPlayer;
+import ddf.minim.Minim;
+
 public class ChordTimeLineApp extends PApplet {
 
 	private static final long serialVersionUID = 6524424444348489408L;
 
-	private String dir = "/Users/bzamecnik/Documents/harmoneye-labs/music-information-retrieval/columbia-music-signal-processing/data/beatles/chordlabs/";
-	private String fileName = dir + "A_Hard_Day_s_Night/05-And_I_Love_Her.lab";
+	private String dir = "/Users/bzamecnik/Documents/harmoneye-labs/music-information-retrieval/columbia-music-signal-processing/data/beatles/";
+	private String labelFileName = dir
+		+ "chordlabs/A_Hard_Day_s_Night/05-And_I_Love_Her.lab";
+	private String audioFileName = dir
+		+ "mp3s-32k/A_Hard_Day_s_Night/05-And_I_Love_Her.mp3";
 
 	private List<TimedChordLabel> chordLabels = new ArrayList<TimedChordLabel>();
 	private double totalTime;
-	private int tonic = 4;
+	private int tonic = 0;
 
 	private TonicDistance tonicDist = new TonicDistance(12);
 
-	private PitchClassNamer pitchClassNamer = PitchClassNamer.defaultInstance();
+	private PitchClassNamer englishFlatNamer = PitchClassNamer
+		.defaultInstance();
+	private PitchClassNamer romanFlatNamer = PitchClassNamer.romanNumeralFlat();
+
+	private Minim minim;
+	private AudioPlayer audioPlayer;
+
+	double currentMillis;
 
 	private String MODE_LINEAR = "LINEAR";
 	private String MODE_FIFTHS = "FIFTHS";
 	private String MODE_TONIC_DIST = "TONIC_DIST";
 
-	private String mode = MODE_TONIC_DIST;
+	private String mode = MODE_FIFTHS;
 	private int diatonicStep = 1;
 
 	private PanZoomController panZoomController;
@@ -58,27 +71,38 @@ public class ChordTimeLineApp extends PApplet {
 		panZoomController = new PanZoomController(this);
 
 		SDrop sdrop = new SDrop(this);
-		loadFile(fileName);
+		minim = new Minim(this);
+		loadAudio(audioFileName);
+		loadChordLabels(labelFileName);
 
 		// println(chordLabels);
 		// println(totalTime);
 
 		colorMode(HSB, 1.0f);
 		smooth();
-
 		noLoop();
 	}
 
 	public void draw() {
+		if (audioPlayer.isPlaying()) {
+			double audioTotalTime = audioPlayer.length() * 0.001;
+			currentMillis = audioPlayer.position() * totalTime / audioTotalTime;
+		}
+		TimedChordLabel currentLabel = labelForTime(0.001 * currentMillis);
+
 		background(0.15f);
 
-		translate(0, height);
-		scale(1, -1);
+		pushMatrix();
 
 		PVector pan = panZoomController.getPan();
 		translate(pan.x, pan.y);
-		float scale = panZoomController.getScale();
-		scale(scale, 1);
+		float scaleX = panZoomController.getScale();
+		// scale(scaleX, 1);
+
+		pushMatrix();
+
+		translate(0, height);
+		scale(1, -1);
 
 		float xScale = (float) (width / totalTime);
 		float yScale = height / 12.0f;
@@ -89,34 +113,34 @@ public class ChordTimeLineApp extends PApplet {
 		noStroke();
 		if (mode.equals(MODE_TONIC_DIST)) {
 			fill(0, 0.2f, 0.15f);
-			rect(0, 7 * yScale, width, 5 * yScale);
+			rect(0, 7 * yScale, scaleX * width, 5 * yScale);
 			fill(0.125f, 0.2f, 0.2f);
-			rect(0, 5 * yScale, width, 2 * yScale);
+			rect(0, 5 * yScale, scaleX * width, 2 * yScale);
 			fill(0.25f, 0.2f, 0.2f);
-			rect(0, 0, width, 5 * yScale);
+			rect(0, 0, scaleX * width, 5 * yScale);
 			fill(0.25f, 0.2f, 0.3f);
-			rect(0, 0, width, yScale);
+			rect(0, 0, scaleX * width, yScale);
 		} else if (mode.equals(MODE_LINEAR)) {
 			for (int i = 0; i < 12; i++) {
 				if (tonicDist.distanceInt(i, 0) < 7) {
 					fill(0.25f, 0.2f, i == 0 ? 0.3f : 0.2f);
-					rect(0, i * yScale, width, yScale);
+					rect(0, i * yScale, scaleX * width, yScale);
 				}
 			}
 		} else if (mode.equals(MODE_FIFTHS)) {
 			fill(0.125f, 0.2f, 0.2f);
-			rect(0, 11 * yScale, width, yScale);
+			rect(0, 11 * yScale, scaleX * width, yScale);
 			fill(0.25f, 0.2f, 0.2f);
-			rect(0, 7 * yScale, width, 4 * yScale);
+			rect(0, 7 * yScale, scaleX * width, 4 * yScale);
 			fill(0.25f, 0.2f, 0.3f);
-			rect(0, 6 * yScale, width, yScale);
+			rect(0, 6 * yScale, scaleX * width, yScale);
 			fill(0.125f, 0.2f, 0.2f);
-			rect(0, 5 * yScale, width, yScale);
+			rect(0, 5 * yScale, scaleX * width, yScale);
 			fill(0, 0.2f, 0.15f);
-			rect(0, 0, width, 5 * yScale);
+			rect(0, 0, scaleX * width, 5 * yScale);
 		}
 
-		// stroke(0);
+		stroke(0);
 
 		for (TimedChordLabel label : chordLabels) {
 			float xStart = (float) (label.getStartTime() * xScale);
@@ -134,12 +158,16 @@ public class ChordTimeLineApp extends PApplet {
 				// } else {
 				// stroke(0);
 				// }
-				// rect(xStart, height*(1-dist), xSize, height);
+				// rect(scaleX*xStart, height*(1-dist), scaleX*xSize, height);
 				int root = chordLabel.getRoot();
+				boolean hightlightEnabled = currentLabel == null
+					|| currentLabel == label;
 				for (Integer tone : tones) {
 					float hue = tonicDist.distanceToHue(tonicDist
 						.distance(tone, tonic));
-					fill(hue, 0.5f, tone == root ? 1 : 0.5f);
+					float brightness = tone == root ? 1 : 0.5f;
+					brightness *= hightlightEnabled ? 1 : 0.8f;
+					fill(hue, 0.5f, brightness);
 					int t = tone;
 					if (mode.equals(MODE_LINEAR)) {
 						t = ((tone - tonic + 12)) % 12;
@@ -162,10 +190,63 @@ public class ChordTimeLineApp extends PApplet {
 					// } else {
 					// stroke(0);
 					// }
-					rect(xStart, t * yScale, xSize, yScale);
+					rect(scaleX * xStart, t * yScale, scaleX * xSize, yScale);
 				}
 			}
 		}
+
+		popMatrix();
+
+		drawPositionMarker();
+
+		popMatrix();
+
+		drawChordTitle(currentLabel);
+	}
+
+	private void drawPositionMarker() {
+		double relativePosition = currentMillis / (totalTime * 1000);
+		float scaleX = panZoomController.getScale();
+		float markerX = (float) (scaleX * width * relativePosition);
+		stroke(0, 0, 1, 0.5f);
+		line(markerX, 0, markerX, height);
+	}
+
+	private void drawChordTitle(TimedChordLabel label) {
+		if (label != null) {
+			String title = relativeChordTitle(label.getChordLabel(), tonic);
+			fill(0, 0, 1, 0.5f);
+			textAlign(CENTER, CENTER);
+			textSize(100);
+			text(title, width / 2, height - 65);
+		}
+	}
+
+	private String relativeChordTitle(ChordLabel chordLabel, int tonic) {
+		if (chordLabel.getTones().isEmpty()) {
+			return "";
+		}
+		String title = chordLabel.getTitle();
+		String[] parts = title.split(":");
+		int root = chordLabel.getRoot();
+		int relativeRoot = (root - tonic + 12) % 12;
+		String rootTitle = romanFlatNamer.getName(relativeRoot);
+		if (parts.length > 1) {
+			rootTitle = rootTitle + ":" + parts[1];
+		}
+		return rootTitle;
+	}
+
+	// O(N), could be O(log(N))
+	private TimedChordLabel labelForTime(double timeSecs) {
+		int length = chordLabels.size();
+		for (int i = 0; i < length; i++) {
+			TimedChordLabel label = chordLabels.get(i);
+			if (label.getEndTime() >= timeSecs) {
+				return label;
+			}
+		}
+		return null;
 	}
 
 	public void keyPressed() {
@@ -180,20 +261,26 @@ public class ChordTimeLineApp extends PApplet {
 		} else if (key == 'm') {
 			if (mode.equals(MODE_LINEAR)) {
 				mode = MODE_FIFTHS;
-				redraw();
 			} else if (mode.equals(MODE_FIFTHS)) {
-				mode = MODE_TONIC_DIST;
-				redraw();
-			} else if (mode.equals(MODE_TONIC_DIST)) {
+				// mode = MODE_TONIC_DIST;
 				mode = MODE_LINEAR;
-				redraw();
 			}
+			// else if (mode.equals(MODE_TONIC_DIST)) {
+			// mode = MODE_LINEAR;
+			// }
 		} else if (key == 'd') {
 			diatonicStep = 1 + (diatonicStep % 2);
-			redraw();
 		} else if (key == 'r') {
 			resetPanZoom();
+		} else if (key == ' ') {
+			togglePlayback();
+		} else if (keyCode == BACKSPACE) {
+			audioPlayer.pause();
+			audioPlayer.rewind();
+			currentMillis = 0;
+			noLoop();
 		}
+		redraw();
 		// panZoomController.keyPressed();
 	}
 
@@ -202,39 +289,46 @@ public class ChordTimeLineApp extends PApplet {
 	}
 
 	public void mouseClicked() {
-		// TODO: fix it for non-zero pan/zoom
-		double time = totalTime * mouseX / width;
-		int labelCount = chordLabels.size();
-		for (int i = 0; i < labelCount; i++) {
-			TimedChordLabel label = chordLabels.get(i);
-			if (label.getEndTime() > time) {
-				ChordLabel chordLabel = label.getChordLabel();
-				List<Integer> tones = chordLabel.getTones();
-				String chordTitle = chordLabel.getTitle();
-				println(time + " " + chordTitle + " " + tones);
-				break;
-			}
+		double totalMillis = 1000 * totalTime;
+		float panX = panZoomController.getPan().x;
+		float scaleX = panZoomController.getScale();
+		double selectedMillis = totalMillis * (mouseX - panX)
+			/ (scaleX * width);
+		double diff = selectedMillis - audioPlayer.position();
+		audioPlayer.skip((int) Math.round(diff));
+		currentMillis = selectedMillis;
+		redraw();
+	}
+
+	private void togglePlayback() {
+		if (audioPlayer.isPlaying()) {
+			audioPlayer.pause();
+			noLoop();
+			redraw();
+		} else {
+			audioPlayer.play();
+			loop();
 		}
 	}
 
 	void shiftTonic(int offset) {
 		setTonic((tonic + offset) % 12);
-		redraw();
 	}
 
-	void loadFile(String fileName) {
+	private void loadChordLabels(String fileName) {
 		if (!fileName.endsWith(".lab")) {
 			println("Unsupported file format: " + fileName);
 			return;
 		}
 
-		this.fileName = fileName;
+		this.labelFileName = fileName;
 		try {
 			Reader reader = createReader(fileName);
 			chordLabels = new ChordLabels().parseTimedChords(reader);
 			if (!chordLabels.isEmpty()) {
 				totalTime = chordLabels.get(chordLabels.size() - 1)
 					.getEndTime();
+				println("labels total time: " + totalTime + " sec");
 				setTonic(keyDetector.findTonic(chordLabels));
 			}
 
@@ -247,6 +341,17 @@ public class ChordTimeLineApp extends PApplet {
 		redraw();
 	}
 
+	private void loadAudio(String fileName) {
+		if (audioPlayer != null) {
+			audioPlayer.pause();
+		}
+		audioPlayer = minim.loadFile(fileName);
+		println("audio total time: " + audioPlayer.length() * 0.001 + " sec");
+		this.audioFileName = fileName;
+		currentMillis = 0;
+		titleChanged();
+	}
+
 	private void resetPanZoom() {
 		panZoomController.setPan(new PVector(0, 0));
 		panZoomController.setScale(1);
@@ -255,20 +360,25 @@ public class ChordTimeLineApp extends PApplet {
 
 	void setTonic(int tonic) {
 		this.tonic = tonic;
-		tonicChanged();
+		titleChanged();
 	}
 
-	void tonicChanged() {
-		frame.setTitle(fileName.replaceAll(".*/", "") + " (key: "
-			+ pitchClassNamer.getName(tonic) + ")");
+	void titleChanged() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(labelFileName.replaceAll(".*/", "")).append(" / ");
+		sb.append(audioFileName.replaceAll(".*/", "")).append(" ");
+		sb.append("(key: " + englishFlatNamer.getName(tonic) + ")");
+		frame.setTitle(sb.toString());
 	}
 
 	public void dropEvent(DropEvent event) {
 		if (event.isFile()) {
-			loadFile(event.toString());
+			String fileName = event.toString();
+			if (fileName.endsWith(".lab")) {
+				loadChordLabels(fileName);
+			} else if (fileName.endsWith(".mp3") || fileName.endsWith(".wav")) {
+				loadAudio(fileName);
+			}
 		}
 	}
-
-	
-
 }
