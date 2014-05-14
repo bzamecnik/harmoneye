1b360b4d913c0cf97e7578ea2bb6146b2af66f9f
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2014-05-14 12:32:23 +0200

    Add interval names.

--- harmoneye-commons/src/main/java/com/harmoneye/music/PitchClassNamer.java
+++ harmoneye-commons/src/main/java/com/harmoneye/music/PitchClassNamer.java
@@ -9,19 +9,27 @@ public class PitchClassNamer {
 	private static PitchClassNamer ENGLISH_SHARP = new PitchClassNamer(
 		new String[] { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A",
 			"A#", "B" });
-	
+
 	private static PitchClassNamer ROMAN_FLAT = new PitchClassNamer(
-		new String[] { "I", "IIb", "II", "IIIb", "III", "IV", "Vb", "V", "VIb", "VI",
-			"VIIb", "VII" });
+		new String[] { "I", "IIb", "II", "IIIb", "III", "IV", "Vb", "V", "VIb",
+			"VI", "VIIb", "VII" });
+
+	private static PitchClassNamer INTERVAL_FLAT = new PitchClassNamer(
+		new String[] { "1", "b2", "2", "b3", "3", "4", "b5", "5", "b6", "6",
+			"b7", "7" });
 
 	private final String[] names;
 
 	public static PitchClassNamer defaultInstance() {
 		return ENGLISH_FLAT;
 	}
-	
+
 	public static PitchClassNamer romanNumeralFlat() {
 		return ROMAN_FLAT;
+	}
+
+	public static PitchClassNamer intervalFlat() {
+		return INTERVAL_FLAT;
 	}
 
 	public PitchClassNamer(String[] names) {
